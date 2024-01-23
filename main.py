63d022bb14c74166aca5b4f20a13cbfacae420f0
SheinyKohan <sheinyk>
2024-01-23 13:56:06 +0200

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from src.app import app
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
-
-
-
+    app.run(host="0.0.0.0", port=8080, debug=True)
--- src/app.py
+++ src/app.py
@@ -2,9 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
 import src.routes
