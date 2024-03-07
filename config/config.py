527f4d2485272cad2b3a6025fe72ff9e747136ec
sheinyk <shbk770@gmail.com>
2024-03-07 22:19:14 +0200

    after clean

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
-
-class Config:
-    # Replace 'your_password' with the actual password
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:6yamim1967@mysql/dbcard'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+#
+# class Config:
+#     # Replace 'your_password' with the actual password
+#     SQLALCHEMY_DATABASE_URI = 'mysql://root:6yamim1967@mysql/dbcard'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
